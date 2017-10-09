d90988a805da91eada1c6c5a59b85d7143232040
Joel Wood <joelmwood@live.com>
2017-10-08 19:00:23 -0500

    Add functionality to ask for user input (name, and hour of sleep), then outputs the user's name and if they got more/less than 8 hours of sleep.

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -6,7 +6,26 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursofSleep = int.Parse(Console.ReadLine());
+
+            if (hoursofSleep > 8)
+            {
+                Console.WriteLine("Hello, " + name + ".");
+                Console.WriteLine("You are well rested.");
+            }
+            else
+            {
+                Console.WriteLine("Hello, " + name + ".");
+                Console.WriteLine("You need more sleep.");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
         }
     }
 }
